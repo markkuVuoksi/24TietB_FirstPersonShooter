a14225882f2d07e4e5645403019dd3fec81e1f36
Juhani-ai <juhani.ahonen@vuoksi.fi>
2025-03-13 12:55:49 +0200

    JA_Grenade_effects

--- Assets/Juhani/JA_Scripts/JA_Grenade.cs
+++ Assets/Juhani/JA_Scripts/JA_Grenade.cs
@@ -9,9 +9,13 @@ public class JA_Grenade : MonoBehaviour
     public float damageAmount = 50f;
     public LayerMask damageableLayer;
     private bool hasExploded = false;
+    public AudioClip soundEffect;
+    private AudioSource audioSource;
+    public ParticleSystem effect;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         StartCoroutine(ExplodeAfterDelay());
     }
 
@@ -24,27 +28,40 @@ public class JA_Grenade : MonoBehaviour
     void Explode()
     {
         if (hasExploded) return;
-
+        TriggerEffect();
+         
         Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius, damageableLayer);
         foreach (Collider nearbyObject in colliders)
-
         {
             Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
             if (rb != null)
-
             {
                 rb.AddExplosionForce(explosionForce, transform.position, blastRadius);
             }
-
             IDamageable damageable = nearbyObject.GetComponent<IDamageable>();
 
             if (damageable != null)
             {
                 damageable.TakeDamage(damageAmount);
             }
-
         }
         hasExploded = true;
         Destroy(gameObject);
+    }
+
+    /*public void PlaySound()
+    {
+        audioSource.PlayOneShot(soundEffect);
+    }
+
+    public void PlayEffect()
+    {
+        effect.Play();
+    }*/
+
+    public void TriggerEffect()
+    {
+       audioSource.PlayOneShot(soundEffect);
+       effect.Play();
     }
 }
