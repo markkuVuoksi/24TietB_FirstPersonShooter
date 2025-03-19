bc5bd84bbc9e310f3c6f6bccbe148cb42ba70d77
Onni-saar <Onni.Saarberg@Vuoksi.fi>
2025-03-19 12:34:23 +0200

    FPS part 1 done.

--- Assets/Onni/OS_Scripts/OS_Enemy.cs
+++ Assets/Onni/OS_Scripts/OS_Enemy.cs
@@ -2,17 +2,6 @@ using UnityEngine;
 
 public class OS_Enemy : MonoBehaviour, OS_IDamageable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     public float health = 100.0f;
     public void TakeDamage(float damageAmount)
     {
