c89445631962b3f5e350292abf7aded8f74b61f9
yuexd07 <mazuninalex1961@gmail.com>
2025-04-08 08:35:25 +0300

    NavMesh for enemies

--- Assets/Aleksandr/Aleksandr_Scripts/Aleksandr_Enemy.cs
+++ Assets/Aleksandr/Aleksandr_Scripts/Aleksandr_Enemy.cs
@@ -1,25 +1,53 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class Aleksandr_Enemy : MonoBehaviour, IDamageableAM
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float health = 100.0f;
 
-    public void TakeDamage(float damageAmount)
+    private NavMeshAgent agent;
+    private float moveTimer;
+    public float moveInterval = 3f;       // Интервал между сменой цели
+    public float wanderRadius = 10f;      // Радиус случайного движения
 
+    void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        moveTimer = moveInterval;
+    }
 
+    void Update()
+    {
+        moveTimer -= Time.deltaTime;
+
+        if (moveTimer <= 0f)
+        {
+            MoveToRandomPosition();
+            moveTimer = moveInterval;
+        }
+    }
+
+    void MoveToRandomPosition()
+    {
+        // Случайное направление от текущей позиции
+        Vector3 randomDirection = Random.insideUnitSphere * wanderRadius;
+        randomDirection += transform.position;
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, 2f, NavMesh.AllAreas))
+        {
+            agent.SetDestination(hit.position);
+        }
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
         health -= damageAmount;
-
         Debug.Log("Enemy health is: " + health);
 
         if (health <= 0)
-
         {
-
             Destroy(gameObject);
-
         }
-
     }
 }
